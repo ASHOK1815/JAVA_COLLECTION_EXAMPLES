7953b704c7ee952eaf8e8cbe800230414396f05e
ASHOK1815 <kumawatashok1815@gmail.com>
2021-07-06 10:47:29 +0530

    class_loader_working_with_small_task

--- javacollection/src/ClassLoaderSubSystem.java
+++ javacollection/src/ClassLoaderSubSystem.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 public class ClassLoaderSubSystem {
 
@@ -7,6 +7,31 @@ public class ClassLoaderSubSystem {
         System.out.println(String.class.getClassLoader());
         //classLoaderSubSystem  is loaded by application subsystem
         System.out.println(ClassLoaderSubSystem.class.getClassLoader());
+
+        Scanner scan=new Scanner(System.in);
+        int age;
+        char ch;
+        do{
+
+          System.out.println("Enter the age");
+          age=scan.nextInt();
+
+          if(age>18)
+          {
+              System.out.println("You can vote");
+          }
+          else
+          {
+              System.out.println("You age is below 18 ,you cannot vote");
+              System.out.println("Come when ");
+          }
+          System.out.println("Do you want to continue .... Press Y else anydigit to quiet");
+          ch=scan.next().charAt(0);
+
+
+        }while(ch=='Y' || ch=='y');
+
+
     }
 
 
